c8c42567f4b1f23e52c07eb52e69d4b52bbc4361
Anti6ot <kradenihs@gmail.com>
2023-06-30 21:05:42 +0500

    homework

--- node-3-module/index.js
+++ node-3-module/index.js
@@ -1,13 +1,46 @@
-const http = require('http')
 const chalk = require('chalk')
+const  path = require('path')
+const express = require('express')
+const {addNote, getNotes, removeNote} = require('./notes.controller')
+
 
 const port = 3000
+const app = express()
 
-const server = http.createServer((reg, res) => {
-    console.log('Server!')
-    res.end('Hello from server')
+app.set('view engine', 'ejs')
+app.set('views', 'pages')
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.get('/', async (req, res)=>{
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: false
+    })
 })
 
-server.listen(port, () => {
+app.post('/', async (req, res)=>{
+    await addNote(req.body.title)
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: true
+    })
+})
+
+app.delete('/:id', async (req, res) => {
+   await removeNote(req.params.id)
+    res.render('index', {
+        title: 'Express App',
+        notes: await getNotes(),
+        created: false
+    })
+})
+
+app.listen(port, () => {
     console.log(chalk.bgGreen(`Server has been started on port ${port}`))
 })
---
+++ node-3-module/public/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('click', event=>{
+    if(event.target.dataset.type === 'remove'){
+        const id = event.target.dataset.id
+
+    remove(id).then(()=> {
+        event.target.closest('li').remove()
+    })
+    }
+    if(event.target.dataset.type === 'redact'){
+        const id = event.target.dataset.id
+        const redactText = prompt('введите новое название')
+        console.log(redact)
+        redact(id )
+    }
+})
+
+async function remove(id) {
+    await fetch(`/${id}`, {method: 'DELETE' })
+}
+async function redact(id ,msg) {
+    await fetch(`/${id}`, {method: 'PUT'})
+}
