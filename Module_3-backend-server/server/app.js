6dc78f6126dfad4ae13dbb6465d3741c73e0a142
Anti6ot <kradenihs@gmail.com>
2023-07-04 19:37:21 +0500

    add models, routes, startUp, mock

--- Module_3-backend-server/server/app.js
+++ Module_3-backend-server/server/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
 const chalk = require("chalk");
+const initDatabase = require('./startUp/initDatabase')
+const routes =require('./routes')
 
 const app = express()
 const PORT = config.get('port') ?? 8080
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
+app.use('/api', routes)
 
 
 // if(process.env.NODE_ENV === 'production'){
@@ -19,9 +21,14 @@ app.use(express.urlencoded({extended:false}))
 
 async function start (){
     try{
+        mongoose.connection.once('open', ()=>{
+            initDatabase()
+        })
+
         await mongoose.connect(config.get('mongoUri'))
+        console.log(chalk.bgGreen(`Mongodb connected`))
         app.listen(PORT, ()=>
-            console.log(chalk.green(`Server has been started on port ${PORT}...`))
+            console.log(chalk.bgGreen(`Server has been started on port ${PORT}...`))
         )
     }catch (e){
         console.log(chalk.red(e.message))
